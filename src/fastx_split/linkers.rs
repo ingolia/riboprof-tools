69d87b57cb1223a65be5eeaea07202f682c8481d
Nicholas Ingolia <nick@ingolia.org>
2018-09-18 21:51:43 -0700

    Tests of linker specification and splitting

--- src/fastx_split/linkers.rs
+++ src/fastx_split/linkers.rs
@@ -237,7 +237,7 @@ mod tests {
 
     const SEQ1: &[u8] = b"ACGTACGTACGTACGT";
     const SEQ2: &[u8] = b"AAAACCCCGGGGTTTT";
-    const SEQ3: &[u8] = b"ATCGATCGATCGATCGAT";
+    const SEQ3: &[u8] = b"CGATCGATCGATCGATCG";
 
     fn fastq(seq: &[u8]) -> fastq::Record
     {
@@ -262,16 +262,67 @@ mod tests {
     }
 
     #[test]
+    fn test_iinn_iinn() {
+        assert_split(SEQ1, "IINN", "IINN", b"GTGT", b"ACAC", b"ACGTACGT", 4+32);
+        assert_split(SEQ2, "IINN", "IINN", b"AATT", b"AATT", b"CCCCGGGG", 4+32);
+        assert_split(SEQ3, "IINN", "IINN", b"ATCG", b"CGAT", b"CGATCGATCG", 4+32);
+        let spec = LinkerSpec::new("IINN", "IINN").unwrap();
+        assert!(spec.prefix_length() == 4);
+        assert!(spec.suffix_length() == 4);
+        assert!(spec.linker_length() == 8);
+        assert!(spec.sample_index_length() == 4);
+        assert!(spec.umi_length() == 4);
+    }
+
+    #[test]
+    fn test_nnii_0() {
+        assert_split(SEQ1, "NNII", "", b"AC", b"GT", b"ACGTACGTACGT", 4+32);
+        assert_split(SEQ2, "NNII", "", b"AA", b"AA", b"CCCCGGGGTTTT", 4+32);
+        assert_split(SEQ3, "NNII", "", b"CG", b"AT", b"CGATCGATCGATCG", 4+32);
+        let spec = LinkerSpec::new("NNII", "").unwrap();
+        assert!(spec.prefix_length() == 4);
+        assert!(spec.suffix_length() == 0);
+        assert!(spec.linker_length() == 4);
+        assert!(spec.sample_index_length() == 2);
+        assert!(spec.umi_length() == 2);
+    }
+
+    #[test]
+    fn test_0_ininnini() {
+        assert_split(SEQ1, "", "ININNINI", b"CTAG", b"AGCT", b"ACGTACGT", 0+32);
+        assert_split(SEQ2, "", "ININNINI", b"GGTT", b"GGTT", b"AAAACCCC", 0+32);
+        assert_split(SEQ3, "", "ININNINI", b"TGAC", b"ACTG", b"CGATCGATCG", 0+32);
+        let spec = LinkerSpec::new("", "ININNINI").unwrap();
+        assert!(spec.prefix_length() == 0);
+        assert!(spec.suffix_length() == 8);
+        assert!(spec.linker_length() == 8);
+        assert!(spec.sample_index_length() == 4);
+        assert!(spec.umi_length() == 4);
+    }
+
+    #[test]
     fn test_nnn_iii() {
         assert_split(SEQ1, "NNN", "III", b"ACG", b"CGT", b"TACGTACGTA", 3 + 32);
         assert_split(SEQ2, "NNN", "III", b"AAA", b"TTT", b"ACCCCGGGGT", 3 + 32);
-        assert_split(SEQ3, "NNN", "III", b"ATC", b"GAT", b"GATCGATCGATC", 3+32);
+        assert_split(SEQ3, "NNN", "III", b"CGA", b"TCG", b"TCGATCGATCGA", 3 + 32);
+        let spec = LinkerSpec::new("NNN", "III").unwrap();
+        assert!(spec.prefix_length() == 3);
+        assert!(spec.suffix_length() == 3);
+        assert!(spec.linker_length() == 6);
+        assert!(spec.sample_index_length() == 3);
+        assert!(spec.umi_length() == 3);
     }
 
     #[test]
     fn test_i_nnnn() {
         assert_split(SEQ1, "I", "NNNN", b"ACGT", b"A", b"CGTACGTACGT", 1+32);
         assert_split(SEQ2, "I", "NNNN", b"TTTT", b"A", b"AAACCCCGGGG", 1+32);
-        assert_split(SEQ3, "I", "NNNN", b"CGAT", b"A", b"TCGATCGATCGAT", 1+32);
+        assert_split(SEQ3, "I", "NNNN", b"ATCG", b"C", b"GATCGATCGATCG", 1+32);
+        let spec = LinkerSpec::new("I", "NNNN").unwrap();
+        assert!(spec.prefix_length() == 1);
+        assert!(spec.suffix_length() == 4);
+        assert!(spec.linker_length() == 5);
+        assert!(spec.sample_index_length() == 1);
+        assert!(spec.umi_length() == 4);
     }
 }
