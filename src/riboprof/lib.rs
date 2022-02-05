d8ff30f30f63b879f3a43abfd676da272571d1cb
Nick Ingolia <nick@ingolia.org>
2022-02-04 16:47:03 -0800

    Remove all non-fastx-split from this branch

--- src/riboprof/lib.rs
+++ src/riboprof/lib.rs
@@ -5,12 +5,5 @@ extern crate regex;
 
 extern crate bio;
 extern crate bio_types;
-extern crate rust_htslib;
 
-pub mod bam_suppress_duplicates;
-pub mod bam_utils;
-pub mod codon_assign;
 pub mod fastx_split;
-pub mod fp_framing;
-pub mod metagene;
-pub mod transcript;
